aa83b60b42c2c60bfdce80da4a98e29eb7074dc3
Your Name <you@example.com>
2025-10-05 23:59:59 +0200

    inshallah jmalloc fix

--- overlays/jemalloc-page-size-16k.nix
+++ overlays/jemalloc-page-size-16k.nix
@@ -1,12 +1,17 @@
-final: prev: {
-  # https://github.com/nvmd/nixos-raspberrypi/issues/64
-  # credit for the initial version of this snippet goes to @micahcc
+(final: prev: {
+  # Patch jemalloc for 16 KB pages (Raspberry Pi 5 / bcm2712)
   jemalloc = prev.jemalloc.overrideAttrs (old: {
-    # --with-lg-page=(log2 page_size)
-    # RPi5 (bcm2712): since our page size is 16384 (2**14), we need 14
-    configureFlags = let
-      pageSizeFlag = "--with-lg-page";
-    in (prev.lib.filter (flag: prev.lib.hasPrefix pageSizeFlag flag == false) old.configureFlags)
-      ++ [ "${pageSizeFlag}=14" ];
+    configureFlags =
+      let
+        pageSizeFlag = "--with-lg-page";
+        # Remove any existing flag, replace with 14 (16 KB)
+        filteredFlags = prev.lib.filter (flag: !(prev.lib.hasPrefix pageSizeFlag flag)) (old.configureFlags or []);
+      in
+        filteredFlags ++ [ "${pageSizeFlag}=14" ];
+
+    # Optional: helps make the change clear in logs
+    meta = old.meta // {
+      description = "${old.meta.description} (patched for 16 KB page size)";
+    };
   });
-}
+})
